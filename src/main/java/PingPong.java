3bbf8eeddc8a950e9004cb489f2ab2da3cd12733
arlen burton and paul hess <pauljacksonhess@gmail.com>
2016-04-18 11:34:00 -0700

    add return of arraylist with passing spec

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+
 public class PingPong {
   public static void main(String[] args) {}
 
-  public Integer runPingPong(Integer yourNumber) {
-    return yourNumber;
+  public ArrayList<Integer> runPingPong(Integer yourNumber) {
+    ArrayList<Integer> returnList = new ArrayList<Integer>();
+    returnList.add(yourNumber);
+    System.out.println(returnList);
+    return returnList;
   }
 }
--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -1,10 +1,20 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.ArrayList;
 
 public class PingPongTest {
+  // @Test
+  // public void PingPong_returnGivenNumber_Integer() {
+  //   PingPong testPingPong = new PingPong();
+  //   assertEquals((Integer)1, testPingPong.runPingPong(1));
+  // }
+
   @Test
-  public void PingPong_returnGivenNumber_Integer() {
+  public void PingPong_countUpToGivenNumber_Array() {
     PingPong testPingPong = new PingPong();
-    assertEquals((Integer)1, testPingPong.runPingPong(1));
+    ArrayList<Integer> expected = new ArrayList<Integer>();
+    expected.add(1);
+    ArrayList<Integer> testReturn = testPingPong.runPingPong(1);
+    assertEquals(expected, testReturn);
   }
 }
